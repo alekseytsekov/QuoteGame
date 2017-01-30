c7be68dee84a338b605101ffaf999e98f04f87ac
Aleksey <alexeitcekov@abv>
2017-01-30 14:11:10 +0200

    adjust nav-bar css

--- FamousQuoteQuizGame/Globals/WorkerSettings.cs
+++ FamousQuoteQuizGame/Globals/WorkerSettings.cs
@@ -3,7 +3,7 @@
     public static class WorkerSettings
     {
         public static string FormKeySaveString = "save changes";
-        public static int MinAttemptsGuess = 0;
+        public static int MinAttemptsGuess = 10;
         public static int MaxStandingList = 15;
         public static int MaxPossibleAnswers = 3;
         public static int MaxAddedQuotesPerDay = 3;
